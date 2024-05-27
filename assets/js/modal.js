322ee74ddb6d805cf18de8bdab1538ab227b5be1
Abel Ciccoli <abelcoli@outlook.fr>
2024-05-27 23:07:51 +0200

    feat(modal):ajout de trucs

---
+++ assets/js/modal.js
@@ -0,0 +1,30 @@
+let triggers = document.querySelectorAll('[data-modal]');
+let modal = document.querySelector('#modal');
+let modalBg = document.querySelector('#modal-bg');
+
+triggers.forEach(m => {
+  m.addEventListener("click", (e) => {
+    toggleModal(m.getAttribute('[data-modal]'))
+  });
+  m.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") toggleModal(m.getAttribute('[data-modal]'));
+  })
+})
+
+
+/**
+*@param {string} modalName
+*/
+function toggleModal(modalName) {
+  if (!modal.classList.contains("modal-left")) {
+    modalBg.removeAttribute("hidden");
+    modal.classList.add("modal");
+    modal.removeAttribute("hidden");
+    htmx.ajax('GET', `/components/${modalName}.html`, '#modal')
+    return;
+  }
+  modalBg.setAttribute("hidden", "true");
+  modal.classList.remove("modal");
+  modal.setAttribute("hidden", "true");
+}
+
