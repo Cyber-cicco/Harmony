3a906f1cdfe2c6c8da7ca8827395106c7c06b751
Abel Ciccoli <abelcoli@outlook.fr>
2024-05-30 00:52:56 +0200

    feat(modal): ajout d'un vrai système de modal. Ajout des modales pour graphisme, exception faite des différents greniers

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,30 +1,41 @@
-let triggers = document.querySelectorAll('[data-modal]');
-let modal = document.querySelector('#modal');
-let modalBg = document.querySelector('#modal-bg');
+function initModal() {
+  let triggers = document.querySelectorAll('[data-modal]');
+  let modal = document.querySelector('#modal');
+  let modalBg = document.querySelector('#modal-bg');
 
-triggers.forEach(m => {
-  m.addEventListener("click", (e) => {
-    toggleModal(m.getAttribute('data-modal'))
-  });
-  m.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") toggleModal(m.getAttribute('[data-modal]'));
+  triggers.forEach(m => {
+    m.addEventListener("click", (e) => {
+      toggleModal(m.getAttribute('data-modal'), modal, modalBg);
+    });
+    m.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") toggleModal(m.getAttribute('data-modal'), modal, modalBg);
+    })
   })
-})
+
+}
 
 
 /**
-*@param {string} modalName
+* @param {string | undefined} modalName
+* @param {Element} modal
+* @param {Element} modalBg
 */
-function toggleModal(modalName) {
-  if (!modal.classList.contains("modal-left")) {
+function toggleModal(modalName, modal, modalBg) {
+  if (!modal.classList.contains("modal")) {
     modalBg.removeAttribute("hidden");
     modal.classList.add("modal");
     modal.removeAttribute("hidden");
-    htmx.ajax('GET', `/components/${modalName}.html`, '#modal')
+    modal.querySelector("#modal-content").setAttribute('src', `./assets/images/${modalName}.png`)
     return;
   }
   modalBg.setAttribute("hidden", "true");
   modal.classList.remove("modal");
   modal.setAttribute("hidden", "true");
+  modal.querySelector("#modal-content").setAttribute('src', './assets/images/graphisme/blank.png')
 }
 
+function closeModal() {
+  let modal = document.querySelector('#modal');
+  let modalBg = document.querySelector('#modal-bg');
+  toggleModal(undefined, modal, modalBg);
+}
