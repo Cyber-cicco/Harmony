58c01591b3a6b1b01dd09c7771f64c0731180000
Abel Ciccoli <abelcoli@outlook.fr>
2024-06-06 18:53:52 +0200

    feat(tout): finalisation de la modale et du carousel

---
+++ assets/js/carousel.js
@@ -0,0 +1,116 @@
+var container = document.querySelector("[data-carousel]");
+var btnLeft = document.querySelector("#cbtn-left");
+var btnRight = document.querySelector("#cbtn-right");
+var elements
+
+function resetElements() {
+  elements = container.querySelectorAll("[data-celement]");
+}
+
+var carouselState = {
+    positionX : 0,
+    index : 0,
+};
+
+function getPosFromIndex(index){
+    return index * -100;
+}
+
+
+function animateCarousel(carouselState, newCarouselState) {
+    for(let el of elements) {
+        el.animate(
+            [
+                // étapes/keyframes
+                { transform: `translateX(${carouselState.positionX}%)` },
+                { transform: `translateX(${newCarouselState.positionX}%)` },
+            ],
+            animationOption
+        )
+    }
+}
+
+var animationOption = { duration: 500, easing: "ease", fill: "forwards" };
+
+function clickRight() {
+    console.log("caca")
+    let newCarouselState
+    if (carouselState.positionX - 100 <= -(elements.length * 100)) {
+        newCarouselState = {
+            positionX : getPosFromIndex(0),
+            index : 0,
+        };
+    } else {
+        newCarouselState = {
+            positionX : carouselState.positionX - 100,
+            index : carouselState.index + 1
+        }
+    }
+
+    animateCarousel(carouselState, newCarouselState);
+    carouselState = newCarouselState
+}
+
+function resetTabulationIndex(oldIndex) {
+    const oldEl = elements[oldIndex];
+    const newEl = elements[carouselState.index];
+    const oldLinks = oldEl.querySelectorAll('a');
+    const oldButtons = oldEl.querySelectorAll('button');
+    const oldIframes = oldEl.querySelectorAll('iframe');
+    const newLinks = newEl.querySelectorAll('a');
+    const newButtons = newEl.querySelectorAll('button');
+    const newIframes = newEl.querySelectorAll('iframe');
+
+    for(let el of oldLinks) {
+        el.setAttribute("tabindex", -1);
+    }
+    for(let el of oldButtons) {
+        el.setAttribute("tabindex", -1);
+    }
+    for(let el of oldIframes) {
+        el.setAttribute("tabindex", -1);
+    }
+    for(let el of newLinks) {
+        el.setAttribute("tabindex", 0);
+    }
+    for(let el of newButtons) {
+        el.setAttribute("tabindex", 0);
+    }
+    for(let el of newIframes) {
+        el.setAttribute("tabindex", 0);
+    }
+}
+
+function clickLeft() {
+
+    let newCarouselState;
+    console.log(carouselState);
+    if (carouselState.positionX === 0) {
+        newCarouselState = {
+            positionX : getPosFromIndex(elements.length - 1),
+            index : elements.length - 1
+        };
+    } else {
+        newCarouselState = {
+            positionX : carouselState.positionX + 100,
+            index : carouselState.index - 1
+        }
+    }
+
+    animateCarousel(carouselState, newCarouselState);
+    carouselState = newCarouselState
+}
+
+
+btnLeft.addEventListener("click", clickLeft);
+btnLeft.addEventListener("keydown",(e) => {
+  if (e.key === "Enter") {
+    clickLeft();
+  }
+});
+btnRight.addEventListener("click", clickRight);
+btnRight.addEventListener("keydown",(e) => {
+  if (e.key === "Enter") {
+    clickRight();
+  }
+});
--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,41 +1,122 @@
+var modalContents
+
 function initModal() {
+  modalContents = new Map()
+  modalContents["graphisme/presentation_grenier_1"] = {
+    type: "carousel",
+    urls: [
+      'graphisme/presentation_grenier_1',
+      'graphisme/presentation_grenier_2',
+      'graphisme/presentation_grenier_3',
+      'graphisme/presentation_grenier_4',
+      'graphisme/presentation_grenier_5',
+    ]
+  }
+  modalContents["graphisme/presentation_roc_bihan"] = {
+    type: "image"
+  }
+  modalContents["graphisme/presentation_charleen"] = {
+    type: "image"
+  }
+  modalContents["graphisme/presentation_brisset"] = {
+    type: "image"
+  }
+
+  modalContents["illustration/01_pearl"] = {
+    type: "image"
+  }
+  modalContents["illustration/02_cassandre"] = {
+    type: "image"
+  }
+  modalContents["illustration/03_opale"] = {
+    type: "image"
+  }
+  modalContents["illustration/04_echo"] = {
+    type: "image"
+  }
+  modalContents["illustration/05_arthur"] = {
+    type: "carousel",
+    urls: [
+      "illustration/05_arthur",
+      "illustration/05_presentation_2",
+    ]
+  }
+  modalContents["illustration/06_films_ta_formation"] = {
+    type: "page",
+    adress: "film"
+  }
+  modalContents["illustration/07_feminisme"] = {
+    type: "image"
+  }
+  modalContents["illustration/08_sorciere"] = {
+    type: "image"
+  }
+  modalContents["illustration/09_burnout"] = {
+    type: "page",
+    adress: "burnout"
+  }
+
   let triggers = document.querySelectorAll('[data-modal]');
   let modal = document.querySelector('#modal');
   let modalBg = document.querySelector('#modal-bg');
 
+
   triggers.forEach(m => {
     m.addEventListener("click", (e) => {
-      toggleModal(m.getAttribute('data-modal'), modal, modalBg);
+      toggleModal(m.getAttribute('data-modal'), modal, modalBg, modalContents);
     });
     m.addEventListener("keydown", (e) => {
-      if (e.key === "Enter") toggleModal(m.getAttribute('data-modal'), modal, modalBg);
+      if (e.key === "Enter") toggleModal(m.getAttribute('data-modal'), modal, modalBg, modalContents);
     })
   })
 
 }
 
-
 /**
+* @typedef {{type:string, urls? : string[], adress? : string}} ModalContent
 * @param {string | undefined} modalName
 * @param {Element} modal
 * @param {Element} modalBg
+* @param {Map<string, ModalContent>} modalContents
 */
-function toggleModal(modalName, modal, modalBg) {
+function toggleModal(modalName, modal, modalBg, modalContents) {
   if (!modal.classList.contains("modal")) {
     modalBg.removeAttribute("hidden");
     modal.classList.add("modal");
     modal.removeAttribute("hidden");
-    modal.querySelector("#modal-content").setAttribute('src', `./assets/images/${modalName}.png`)
+    const contentOpts = modalContents[modalName];
+    switch (contentOpts.type) {
+      case 'image':
+        modal.querySelector("#modal-content").innerHTML = `
+          <img data-celement="true" class="min-w-full" src="../../assets/images/${modalName}.png">
+          `
+        break;
+      case 'carousel':
+        modal.querySelectorAll("[data-carousel-control]").forEach((el) => {
+          el.removeAttribute("hidden")
+        })
+        modal.querySelector("#modal-content").innerHTML = contentOpts.urls.map((url) => {
+          return `<img data-celement="true" class="min-w-full" src="../../assets/images/${url}.png">`
+        }).join('')
+        break;
+      case 'page':
+        console.log("caca")
+        htmx.ajax('GET', `./components/${contentOpts.adress}.html`, '#modal-content')
+    }
+    resetElements()
     return;
   }
   modalBg.setAttribute("hidden", "true");
   modal.classList.remove("modal");
+  modal.querySelectorAll("[data-carousel-control]").forEach((el) => {
+    el.setAttribute("hidden", "true")
+  })
   modal.setAttribute("hidden", "true");
-  modal.querySelector("#modal-content").setAttribute('src', './assets/images/graphisme/blank.png')
+  modal.querySelector("#modal-content").innerHTML = `<img class="w-full" src="../../assets/images/graphisme/blank.png">`
 }
 
 function closeModal() {
   let modal = document.querySelector('#modal');
   let modalBg = document.querySelector('#modal-bg');
-  toggleModal(undefined, modal, modalBg);
+  toggleModal(undefined, modal, modalBg, modalContents);
 }
